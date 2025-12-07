560bf9529523e458cd1d5c25064dadea7fe94184
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-12-07 01:34:45 +0100

    components dir added, Navbar and Footer component added

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold">Om mig</h1>
+      <p className="mt-4 max-w-xl">
+        Jag heter Robin och studerar till frontendutvecklare. Jag gillar React,
+        TypeScript och att bygga tydliga, användarvänliga gränssnitt.
+      </p>
+    </main>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold">Kontakta mig</h1>
+      <p className="mt-4 max-w-xl">Kontakta mig genom att fylla i formuläret</p>
+    </main>
+  );
+}
